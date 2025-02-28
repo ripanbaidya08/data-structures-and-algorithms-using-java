c39ddc0c2e45cf4ce6ea70fcedd8e039e75e15de
ripan baidya <ripanbaidya7980@gmail.com>
2025-02-28 10:11:22 +0530

    700. Search in a Binary Search Tree

--- src/io/dsalgo/binarysearchtree/problems/easy/SearchInBST.java
+++ src/io/dsalgo/binarysearchtree/problems/easy/SearchInBST.java
@@ -4,16 +4,38 @@ import io.dsalgo.binarysearchtree.implementation.TreeNode;
 
 // 700. Search in a Binary Search Tree
 class SearchInBST {
-    public TreeNode searchBST(TreeNode root, int val) {
+    public static TreeNode searchBST(TreeNode root, int val) {
         if(root == null) return null;
 
         if(root.val == val) {
             return root;
         } else if(val < root.val){
             root = searchBST(root.left, val);
-        } else {
+        } else { // (val > root.val)
             root = searchBST(root.right, val);
         }
         return root;
+    }
+
+    public static void main(String[] args) {
+        /*
+            4
+           / \
+          2   7
+         / \
+        1  3
+
+        * target = 2.
+        */
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        int val = 2;
+
+        TreeNode res = searchBST(root, val);
+        System.out.println(res.val);
     }
 }
